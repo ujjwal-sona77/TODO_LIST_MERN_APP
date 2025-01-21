f5b28f8e9879a9ad658c140aa06666c57be43918
Ujjwal Sonawat <ujjwalsonawat7@gmail.com>
2025-01-21 18:55:56 +0530

    done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import Home from "./components/Home";
-import ProtectedRoute from "./components/ProtectedRoute";
 import NewTodo from "./components/NewTodo";
 
 const App = () => {
