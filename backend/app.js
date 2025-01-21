b831d6f99d058f997a5b86299be3621493402931
Ujjwal Sonawat <146330690+ujjwal-sona77@users.noreply.github.com>
2025-01-21 18:38:04 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(cors({
     credentials: true,
-    origin: "http://localhost:5173"
+    origin: "https://todo-list-phi-eight-89.vercel.app"
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
