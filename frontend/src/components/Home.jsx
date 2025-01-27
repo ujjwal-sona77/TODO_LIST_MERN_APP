c4a36f65ec13682d93a3da421cb63e595aba0a51
Ujjwal Sonawat <ujjwalsonawat7@gmail.com>
2025-01-27 17:58:25 +0530

    changed

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -117,7 +117,7 @@ return (
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-20">
             <div className="flex justify-end mb-6">
                 <Link 
-                    to="/create-todo"
+                    to="/newtodo"
                     className="bg-blue-500 text-white px-4 py-2 rounded-full transition duration-300 ease-in-out transform hover:scale-105"
                 >
                     Create Todo +
